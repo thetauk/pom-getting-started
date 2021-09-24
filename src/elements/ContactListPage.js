4498b512bf80eff26c65f12e6137d75e9e8585c5
Ranvir Chhina <ranvirchhina25@gmail.com>
2021-09-23 22:22:11 -0700

    Removing tsc compiled files

--- src/elements/ContactListPage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContactsListPageElements = void 0;
-exports.ContactsListPageElements = {
-    floatingActionButton: "id:com.android.contacts:id/floating_action_button",
-};
--- src/elements/CreateContactPage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateContactPageElements = void 0;
-exports.CreateContactPageElements = {
-    firstNameField: 'android=new UiSelector().text("First name")',
-    lastNameField: 'android=new UiSelector().text("Last name")',
-    saveButton: "id:com.android.contacts:id/editor_menu_save_button",
-};
